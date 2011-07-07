896a63cc46d696a4c34efd209a87878dd6f34dbd
Jai-Gouk Kim <jaigouk@gmail.com>
2011-07-08 06:18:00 +0900

    cleaned up codes

--- coffee_files/014_0_tools.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  var http;
-  http = require('http');
-  http.createServer(function(req, res) {
-    console.log('%s %s', req.method, req.url);
-    res.writeHead(200);
-    return res.end('Hello World2');
-  }).listen(4000);
-}).call(this);
